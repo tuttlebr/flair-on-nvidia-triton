109ba71a8f3114a3bec0acbd4f2eb9bd6025f0c1
Brandon Tuttle <tuttlebr@ncc-1701.biz>
2022-11-17 00:28:08 +0000

    decouple tokenizer and model
    Signed-off-by: Brandon Tuttle <tuttlebr@ncc-1701.biz>

--- workspace/bert_model_surgery.py
+++ workspace/bert_model_surgery.py
@@ -1,49 +1,42 @@
 import torch
-from transformers import BertModel, BertTokenizer
+from transformers import BertTokenizer, BertModel
 
 
-class CustomModel(torch.nn.Module):
+text_0 = "Who was Jim Henson?"
+text_1 = "Jim Henson was a puppeteer"
+
+
+class TritonBertModel(torch.nn.Module):
     def __init__(self):
-        super(CustomModel, self).__init__()
+        super(TritonBertModel, self).__init__()
         self.model = BertModel.from_pretrained(
             "bert-base-uncased",
             torchscript=True,
-            output_attentions=True).to("cuda:0")
-        self.tokenizer = BertTokenizer.from_pretrained(
-            "bert-base-uncased", torchscript=True)
+            output_attentions=True)
 
-    def dummy_input(self):
-        text_1 = "Who was Jim Henson ?"
-        text_2 = "Jim Henson was a puppeteer"
-        INPUT__0 = torch.ByteTensor(list(bytes(text_1, "utf8"))).to("cuda:0")
-        INPUT__1 = torch.ByteTensor(list(bytes(text_2, "utf8"))).to("cuda:0")
-        return INPUT__0, INPUT__1
-
-    def forward(self, INPUT__0, INPUT__1):
-        text_0 = bytes(INPUT__0).decode()
-        tokenized_text_0 = self.tokenizer.encode(
-            text=text_0, add_special_tokens=True, return_tensors="pt")
-        segment_0 = torch.zeros(tokenized_text_0.shape, dtype=torch.int64)
-
-        text_1 = bytes(INPUT__1).decode()
-        tokenized_text_1 = self.tokenizer.encode(
-            text=text_1, add_special_tokens=True, return_tensors="pt")
-        segment_1 = torch.ones(tokenized_text_1.shape, dtype=torch.int64)
-
-        tokens_tensor = torch.concat(
-            (tokenized_text_0, tokenized_text_1), axis=1).to("cuda:0")
-        segments_tensors = torch.concat(
-            (segment_0, segment_1), axis=1).to("cuda:0")
-        return self.model(
-            tokens_tensor,
-            token_type_ids=segments_tensors)[0].squeeze()
+    def forward(self, tokens_tensor, segments_tensors):
+        return self.model(tokens_tensor, segments_tensors)[0]
 
 
-model = CustomModel().eval()
-INPUT__0, INPUT__1 = model.dummy_input()
-OUTPUT__0 = model.forward(INPUT__0, INPUT__1)
-traced_model = torch.jit.trace(model, example_inputs=[INPUT__0, INPUT__1])
-torch.jit.save(
-    traced_model, "/workspace/triton-models/bert-base-uncased/1/model.pt")
-
-print("saved TorchScript model as /workspace/triton-models/bert-base-uncased/1/model.pt")
+if __name__ == '__main__':
+    tokenizer = BertTokenizer.from_pretrained(
+        "bert-base-uncased", torchscript=True)
+    tokenized_tensor_0 = tokenizer(
+        text_0,
+        add_special_tokens=True,
+        return_tensors="pt")
+    tokenized_tensor_1 = tokenizer(
+        text_1,
+        add_special_tokens=True,
+        return_tensors="pt")
+    tokens_tensor = torch.concat(
+        (tokenized_tensor_0["input_ids"],
+         tokenized_tensor_1["input_ids"]),
+        axis=1)
+    segments_tensors = torch.concat(
+        (tokenized_tensor_0["token_type_ids"],
+         tokenized_tensor_1["attention_mask"]),
+        axis=1)
+    model = TritonBertModel().eval()
+    traced_model.save("/workspace/triton-models/bert-base-uncased/1/model.pt")\
+        print("saved TorchScript model as /workspace/triton-models/bert-base-uncased/1/model.pt")
